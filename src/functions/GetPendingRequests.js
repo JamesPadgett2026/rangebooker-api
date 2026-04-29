6d11b80646c6e7436ade918b2ae613de53deaede
JamesPadgett2026 <jpadgett@tropicaltech.com>
2026-04-28 22:20:57 -0400

    Update GetPendingRequests.js

--- src/functions/GetPendingRequests.js
+++ src/functions/GetPendingRequests.js
@@ -4,13 +4,18 @@ app.http("GetPendingRequests", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: async (request, context) => {
-
     const data = [
       {
-        MemberEmailColSP: "james@example.com",
+        MemberEmailColSP: "acousticjohnson.aj@gmail.com",
         DateIDLOckInColSP: "2026-05-01",
         DateRequestWasAdded: "2026-04-28T20:14:00",
         StatusColSP: "Pending"
+      },
+      {
+        MemberEmailColSP: "acousticjohnson.aj@gmail.com",
+        DateIDLOckInColSP: "2026-05-03",
+        DateRequestWasAdded: "2026-04-27T18:10:00",
+        StatusColSP: "Approved"
       }
     ];
 
