0324fbf2452859821e8ef572fbe74fed0923a029
JamesPadgett2026 <jpadgett@tropicaltech.com>
2026-04-28 21:42:27 -0400

    Update GetPendingRequests.js

--- src/functions/GetPendingRequests.js
+++ src/functions/GetPendingRequests.js
@@ -1,16 +1,22 @@
-module.exports = async function (context, req) {
+const { app } = require("@azure/functions");
+
+app.http("GetPendingRequests", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
 
     const data = [
-        {
-            MemberEmailColSP: "james@example.com",
-            DateIDLOckInColSP: "2026-05-01",
-            DateRequestWasAdded: "2026-04-28T20:14:00",
-            StatusColSP: "Pending"
-        }
+      {
+        MemberEmailColSP: "james@example.com",
+        DateIDLOckInColSP: "2026-05-01",
+        DateRequestWasAdded: "2026-04-28T20:14:00",
+        StatusColSP: "Pending"
+      }
     ];
 
-    context.res = {
-        status: 200,
-        body: data
+    return {
+      status: 200,
+      jsonBody: data
     };
-};
+  }
+});
